e7692608f6e689149dd7cdd80fd7f0af4142f081
Paul Aschmann <paul@paups.com>
2014-04-26 02:51:30 -0400

    Initial Commit

---
+++ Hack2014/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hack2014
+//
+//  Created by Paul Aschmann on 4/26/14.
+//  Copyright (c) 2014 Lithium Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hack2014/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hack2014
+//
+//  Created by Paul Aschmann on 4/26/14.
+//  Copyright (c) 2014 Lithium Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
